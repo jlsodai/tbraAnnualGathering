6a58555e5ff611a9a4c8b5435894f27116d6b02c
Jules Maestro <jlsodai@gmail.com>
2023-05-04 09:31:43 +0000

    add footer section

---
+++ src/app/sections/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <section className="bg-tender text-white">
+      <div className="container py-8 text-center">
+        <p>© Copyright 2023 — TheBoardroom Africa. All Rights Reserved</p>
+      </div>
+    </section>
+  )
+}
+
+export default Footer

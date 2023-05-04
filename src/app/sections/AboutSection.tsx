d0e384960d5ade94c3106f3693e93f0a6e113ea4
Jules Maestro <jlsodai@gmail.com>
2023-05-04 13:51:37 +0000

    replace buy ticket with modal button

--- src/app/sections/AboutSection.tsx
+++ src/app/sections/AboutSection.tsx
@@ -1,14 +1,17 @@
 /* eslint-disable @next/next/no-img-element */
-import React from 'react'
+import Modal from '@/app/components/Modal'
+import PaperForm from '@/app/components/PaperForm'
 
 const AboutSection = () => {
   return (
-    <section className="bg-mustard-50 min-h-screen flex">
+    <section id="overview" className="bg-mustard-50 min-h-screen flex">
       <div className="container grid md:grid-cols-2 gap-8 md:gap-20 items-center -md:my-16">
         <div className="md:pr-16">
           <h3 className="font-libreb text-4xl">About the annual gathering</h3>
           <p className="mt-8">Quas laborum aliquam veritatis, molestiae illum repellendus natus dolores excepturi incidunt ex rem qui. Dolores, rerum et iusto nobis voluptatem totam modi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas laborum aliquam veritatis, molestiae illum repellendus natus dolores excepturi incidunt ex rem qui. Dolores, rerum et iusto nobis voluptatem totam modi?</p>
-          <a href="#!" className="btn text-black mt-8">Buy Ticket</a>
+          <Modal btnText="Buy Ticket">
+						<PaperForm formSlug="standardadvertisement" />
+					</Modal>
         </div>
         <img src="/images/egyptPyramids.svg" alt="Egypt Pyramid" className="-sm:order-first" />
       </div>
